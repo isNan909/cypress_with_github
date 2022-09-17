e7b4c67ae7ac217e634f1855e1466cb707967b6e
ishan-me <ishan02016@gmail.com>
2022-09-17 16:34:36 +0545

    added pRllel execution of test

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173/",
+    projectId: "ozsx7a",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
