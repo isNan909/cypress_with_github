506bcc04331b4916aa57306d0c2e412dc378c99a
coderVSL <virajlakshan488@gmail.com>
2024-03-13 15:01:03 +0530

    add comment to cypress file

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     baseUrl: "http://localhost:5173/",
     projectId: "ozsx7a",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here // added a comment  to the cypress file 
     },
   },
 });
